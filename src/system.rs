b2bbeed43417ba7c54c0d908601d7702a6664d9e
Wlegby <gigamullman@gmail.com>
2026-06-12 22:42:04 +0200

    removed anki launch printing

--- src/system.rs
+++ src/system.rs
@@ -1,10 +1,12 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 /// Launches the Anki application on the host machine.
 ///
 /// This simply runs the `anki` command in the background. It assumes `anki` is in your PATH.
 pub fn launch_anki() -> std::io::Result<()> {
     Command::new("anki")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
         // We spawn it detached so our Rust program isn't blocked and doesn't wait for Anki to exit.
         .spawn()?;
 
